f011e479e81d171b166b21036bba3806b630a839
Anastasiia <a.khrypko.work@gmail.com>
2024-08-19 20:00:00 +0200

    NoteSchema is created

---
+++ backend/models/note.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const noteSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    title: String,
+    content: String,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+    },
+});
+
+export const NoteModel = model("Note", noteSchema, "notes");
