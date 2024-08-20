d7d0b4379ff35e6cdd119d45cbd34b2c9849449d
Anastasiia <a.khrypko.work@gmail.com>
2024-08-20 05:19:14 +0200

    UserContext added, step-by-step input implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,48 @@
-import SignUpComponent from './components/SignUp.component.jsx';
-import SignInComponent from './components/SignIn.component.jsx';
-import CreateNoteComponent from './components/CreateNote.component.jsx';
-import NotesComponent from './components/Notes.component.jsx';
-import './App.scss';
+import { useState, useEffect } from "react";
+import WelcomeComponent from "./components/Welcome.component.jsx";
+import AuthComponent from "./components/Auth.component.jsx";
+import UserMenuComponent from "./components/UserMenu.component.jsx";
+import NotesComponent from "./components/Notes.component.jsx";
+import CreateNoteComponent from "./components/CreateNote.component.jsx";
+import { UserProvider } from "./contexts/User.context.jsx";
+import "./App.scss";
 
 function App() {
+    const [step, setStep] = useState(0);
 
-  return (
-    <>
-      <h1>MindPad</h1>
-      <SignUpComponent />
-      <SignInComponent />
-      <CreateNoteComponent />
-      <NotesComponent />
-    </>
-  )
+    useEffect(() => {
+      const userId = localStorage.getItem('userId');
+      if (userId) {
+        setStep(2);
+      }
+    }, []);
+
+    const handleStart = () => {
+        setStep(1);
+    };
+
+    const handleAuthSuccess = () => {
+        setStep(2);
+    };
+
+    const handleLogout = () => {
+      setStep(1);
+    };
+
+    return (
+        <UserProvider>
+            <h1>MindPad</h1>
+            {step === 0 && <WelcomeComponent onStart={handleStart} />}
+            {step === 1 && <AuthComponent onAuthSuccess={handleAuthSuccess} />}
+            {step === 2 && (
+              <>
+                <UserMenuComponent onLogout={handleLogout} />
+                <NotesComponent />
+                <CreateNoteComponent />
+              </>
+            )}
+        </UserProvider>
+    );
 }
 
 export default App;
