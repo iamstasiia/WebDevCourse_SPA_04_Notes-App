71af1d2241e18a2f2e2ce72e4da4ea68e9a262f6
Anastasiia <a.khrypko.work@gmail.com>
2024-08-27 13:49:07 +0200

    added user password hashing

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,9 +1,40 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+        minlength: 2,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// userSchema.pre("save", async function (next) {
+//     try {
+//         if (!this.isModified("password")) {
+//             return next();
+//         }
+//         // const salt = await bcrypt.genSalt(10);
+//         this.password = await bcrypt.hash(this.password, 10);
+//         next();
+//     } catch (error) {
+//         return next(error);
+//     }
+// });
+
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
 });
 
 export const UserModel = model("User", userSchema, "users");
