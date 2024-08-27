3883878d546e16baf3b3f6ec16d501e1f91937b3
Anastasiia <a.khrypko.work@gmail.com>
2024-08-27 14:34:18 +0200

    login has been changed to a hashed password

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const userRegisterController = async (req, res, next) => {
     try {
@@ -17,21 +18,29 @@ export const userLoginController = async (req, res, next) => {
     const { email, password } = req.body;
 
     try {
-        const user = await UserModel.findOne({ email, password });
+        const user = await UserModel.findOne({ email });
 
-        if (user) {
-            res.status(200).json({
-                code: 200,
-                answer: "User successfully found.",
-                userId: user._id,
-                username: user.username,
-            });
-        } else {
-            res.status(404).json({
+        if (!user) {
+            return res.status(404).json({
                 code: 404,
                 answer: "User not found.",
             });
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({
+                code: 401,
+                answer: "Invalid password.",
+            });
+        }
+
+        res.status(200).json({
+            code: 200,
+            answer: "User successfully found.",
+            userId: user._id,
+            username: user.username,
+        });
     } catch (error) {
         next(error);
     }
