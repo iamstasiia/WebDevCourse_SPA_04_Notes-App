2afd1b179b152f600bd83f0a30863537bb53b509
Anastasiia <a.khrypko.work@gmail.com>
2024-09-01 20:30:58 +0200

    menu icons added

--- frontend/src/components/header/UserMenu.component.jsx
+++ frontend/src/components/header/UserMenu.component.jsx
@@ -1,22 +1,34 @@
 import { useContext } from "react";
-// import { UserContext } from '../contexts/User.context.js';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/Auth.context.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faArrowRightFromBracket,
+    faHouse,
+    faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { Link, useLocation } from "react-router-dom";
-const UserMenuComponent = ({ onLogout }) => {
+const UserMenuComponent = () => {
     const location = useLocation();
     const isInTrash = location.pathname === "/notes/trash";
 
-    const { setUserId } = useContext(AuthContext);
+    const { logout } = useContext(AuthContext);
     const username = localStorage.getItem("username");
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         localStorage.removeItem("userId");
         localStorage.removeItem("username");
-        setUserId(null);
-        onLogout();
+        logout();  
+        navigate('/auth/login');      
+    };
+
+    const handleToTrash = () => {
+        navigate('trash');
+    };
+
+    const handleToNotes = () => {
+        navigate('/notes');
     };
 
     return (
@@ -24,15 +36,22 @@ const UserMenuComponent = ({ onLogout }) => {
             <h2>Hello, {username}</h2>
             <nav>
                 {isInTrash ? (
-                    <Link to="/notes">Back to Notes</Link>
+                    <button className="tooltip" onClick={handleToNotes}>
+                            <FontAwesomeIcon icon={faHouse} />
+                        <span className="tooltiptext">To Notes</span>
+                    </button>
                 ) : (
-                    <Link to="trash">Go to Trash</Link>
+                    <button className="tooltip" onClick={handleToTrash}>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        <span className="tooltiptext">To Trash</span>
+                    </button>
                 )}
+
+                <button className="tooltip" onClick={handleLogout}>
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                    <span className="tooltiptext">Log out</span>
+                </button>
             </nav>
-            <button className="tooltip" onClick={handleLogout}>
-                <FontAwesomeIcon icon={faArrowRightFromBracket} />
-                <span className="tooltiptext">Log out</span>
-            </button>
         </header>
     );
 };
